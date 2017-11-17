64199f8fc88cf5f7fe230a60336d1bc5db4d7b0c
derek <derek@butterfly.sg>
2017-11-17 16:47:05 +0800

    Init

---
+++ SwiftStepView/Sources/StepView.swift
@@ -0,0 +1,17 @@
+//
+//  StepView.swift
+//  SwiftStepView
+//
+//  Created by Julie on 17/11/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+import UIKit
+
+class StepView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
---
+++ SwiftStepView/SwiftStepView.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStepView.h
+//  SwiftStepView
+//
+//  Created by Julie on 17/11/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftStepView.
+FOUNDATION_EXPORT double SwiftStepViewVersionNumber;
+
+//! Project version string for SwiftStepView.
+FOUNDATION_EXPORT const unsigned char SwiftStepViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStepView/PublicHeader.h>
+
+
